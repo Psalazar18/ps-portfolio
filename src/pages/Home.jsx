2166653d06b254ee263af5ac52f07caa9a13e528
Psalazar18 <paolasalazar90@gmail.com>
2025-06-12 14:14:07 -0400

    contenido y estilos del home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import './Home.css'
 
-function Home () {
+function Home() {
     return (
         <>
-        <main className="main">
-            <h1>WEB UI DEVELOPER</h1>
-        </main>
+            <main className="main">
+                <div className="container">
+                    <div className="main-text text-end">
+                        <h1>¡Hola! <br/> soy Paola</h1>
+                        <p>Soy una desarrolladora web con ojo para los detalles y una gran curiosidad por cómo hacer que las ideas cobren vida en la pantalla. Me apasiona crear experiencias digitales que conecten, sorprendan y funcionen. Pero no solo vivo en el mundo del código: también traigo conmigo experiencia en atención al cliente, ventas y otras áreas que me han enseñado a trabajar en equipo, resolver problemas y adaptarme rápido.
+
+                        Este es mi espacio para mostrarte lo que he hecho y todo lo que puedo llegar a hacer. Siempre lista para un nuevo desafío.</p>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
