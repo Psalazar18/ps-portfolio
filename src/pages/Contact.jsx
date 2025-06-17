46519528031747b764c88c8e6d60eabde75f071a
Psalazar18 <paolasalazar90@gmail.com>
2025-06-17 16:05:04 -0400

    cambio de diseño y accesibilidad del contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,19 +4,28 @@ import './Contact.css'
 function Contact() {
     return (
         <>
-            <section className="contact">
+            <section className="contact" aria-labelledby="contact-heading">
                 <div className="container">
                     <h1 className="">Contact</h1>
-                    <div className="cv-container d-flex flex-row">
+                    <div className="cv-container d-flex flex-row" role="group" aria-label="Descarga de currículum">
                         <div className="cv-box">
-                            <embed src="/pdfs/CV-PaolaSalazar-2025_esp.pdf" type="application/pdf" className="cv-preview" />
-                            <a href="/pdfs/CV-PaolaSalazar-2025_esp.pdf" download className="cv-download-btn">
+                            <a 
+                                href="/pdfs/CV-PaolaSalazar-2025_esp.pdf" 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className="cv-download-btn"
+                                aria-label="Descargar CV en español en formato PDF">
+                                
                                 Descargar CV (ES)
                             </a>
                         </div>
                         <div className="cv-box">
-                            <embed src="/pdfs/CV-PaolaSalazar-2025_eng.pdf" type="application/pdf" className="cv-preview" />
-                            <a href="/pdfs/CV-PaolaSalazar-2025_eng.pdf" download className="cv-download-btn">
+                            <a 
+                                href="/pdfs/CV-PaolaSalazar-2025_eng.pdf" 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className="cv-download-btn"
+                                aria-label="Download resume in English in PDF format">
                                 Download CV (EN)
                             </a>
                         </div>
